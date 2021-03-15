55fc674cdbc564c11790c9f85a3353e33f548127
carl-alphonce <alphonce@buffalo.edu>
2021-03-14 21:53:08 -0400

    Added comments.

--- Date.h
+++ Date.h
@@ -3,14 +3,18 @@
 
 #include <stdbool.h>
 
+// A representation of a date
 struct Date {
   int date;
   int month;
   int year;
 };
 
+// Returns a Date struct representing the day after the argument date
 struct Date * nextDay(struct Date * day);
 
+// Returns true if d1 and d2 represent the same date, 
+//         false otherwise
 bool sameDay(struct Date * d1, struct Date * d2);
 
 #endif
